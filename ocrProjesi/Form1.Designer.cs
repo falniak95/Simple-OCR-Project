122ae42b1dac95037ac391741cc70c111e8be466
Your Name <falniak95@gmail.com>
2018-01-18 00:33:49 +0300

    Proje github'a eklendi.

--- ocrProjesi/Form1.Designer.cs
+++ ocrProjesi/Form1.Designer.cs
@@ -28,12 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(291, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 270);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 237);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 384);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- ocrProjesi/Form1.cs
+++ ocrProjesi/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tesseract;
+using AForge.Imaging.Filters;
 
 namespace ocrProjesi
 {
@@ -15,6 +17,58 @@ namespace ocrProjesi
         public Form1()
         {
             InitializeComponent();
+        }
+        private string OCR(Bitmap b)
+        {
+            string res = "";
+            using (var engine = new TesseractEngine(@"tessdata", "eng", EngineMode.Default))
+            {
+                engine.SetVariable("tessedit_char_whitelist", "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+                engine.SetVariable("tessedit_unrej_any_wd", true);
+
+                using (var page = engine.Process(b, PageSegMode.SingleLine))
+                    res = page.GetText();
+            }
+            return res;
+        }
+
+        private string reconhecerCaptcha(Image img)
+        {
+            Bitmap _bitmap = new Bitmap(img);
+            _bitmap = _bitmap.Clone(new Rectangle(0, 0, img.Width, img.Height), System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            Erosion erosion = new Erosion();
+            Dilatation dilatation = new Dilatation();
+            Invert inverter = new Invert();
+            ColorFiltering cor = new ColorFiltering();
+            cor.Blue = new AForge.IntRange(200, 255);
+            cor.Red = new AForge.IntRange(200, 255);
+            cor.Green = new AForge.IntRange(200, 255);
+            Opening open = new Opening();
+            BlobsFiltering bc = new BlobsFiltering();
+            Closing close = new Closing();
+            GaussianSharpen gs = new GaussianSharpen();
+            ContrastCorrection cc = new ContrastCorrection();
+            bc.MinHeight = 10;
+            FiltersSequence seq = new FiltersSequence(gs, inverter, open, inverter, bc, inverter, open, cc, cor, bc, inverter);
+            pictureBox1.Image = seq.Apply(_bitmap);
+            string recognedString = OCR((Bitmap)pictureBox1.Image);
+            return recognedString;
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            Bitmap image = new Bitmap(openFileDialog1.FileName);
+            if (image != null)
+            {
+                System.IO.FileInfo fi = new System.IO.FileInfo(openFileDialog1.FileName);
+                label1.Text = reconhecerCaptcha(image);
+            }
+            image.Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
         }
     }
 }
